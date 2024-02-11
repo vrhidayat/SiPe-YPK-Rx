12c61281ed4270c4f05d965a8bfa38478f9a4b8c
fadvr <fadnoor31@gmail.com>
2024-02-11 12:09:03 +0700

    add for crystal report

---
+++ CRView.Designer.vb
@@ -0,0 +1,78 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class CRView
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.CrystalReportViewer1 = New CrystalDecisions.Windows.Forms.CrystalReportViewer()
+        Me.dtpStart = New System.Windows.Forms.DateTimePicker()
+        Me.dtpEnd = New System.Windows.Forms.DateTimePicker()
+        Me.Report1 = New SiPe_YPK_Rx.Report()
+        Me.SuspendLayout()
+        '
+        'CrystalReportViewer1
+        '
+        Me.CrystalReportViewer1.ActiveViewIndex = 0
+        Me.CrystalReportViewer1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.CrystalReportViewer1.Cursor = System.Windows.Forms.Cursors.Default
+        Me.CrystalReportViewer1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.CrystalReportViewer1.Location = New System.Drawing.Point(0, 0)
+        Me.CrystalReportViewer1.Name = "CrystalReportViewer1"
+        Me.CrystalReportViewer1.ReportSource = Me.Report1
+        Me.CrystalReportViewer1.Size = New System.Drawing.Size(914, 492)
+        Me.CrystalReportViewer1.TabIndex = 0
+        '
+        'dtpStart
+        '
+        Me.dtpStart.Format = System.Windows.Forms.DateTimePickerFormat.[Short]
+        Me.dtpStart.Location = New System.Drawing.Point(498, 6)
+        Me.dtpStart.Name = "dtpStart"
+        Me.dtpStart.Size = New System.Drawing.Size(80, 20)
+        Me.dtpStart.TabIndex = 1
+        '
+        'dtpEnd
+        '
+        Me.dtpEnd.Format = System.Windows.Forms.DateTimePickerFormat.[Short]
+        Me.dtpEnd.Location = New System.Drawing.Point(584, 6)
+        Me.dtpEnd.Name = "dtpEnd"
+        Me.dtpEnd.Size = New System.Drawing.Size(80, 20)
+        Me.dtpEnd.TabIndex = 2
+        '
+        'CRView
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(914, 492)
+        Me.Controls.Add(Me.dtpEnd)
+        Me.Controls.Add(Me.dtpStart)
+        Me.Controls.Add(Me.CrystalReportViewer1)
+        Me.Name = "CRView"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "FormLaporan"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents CrystalReportViewer1 As CrystalDecisions.Windows.Forms.CrystalReportViewer
+    Friend WithEvents dtpStart As DateTimePicker
+    Friend WithEvents dtpEnd As DateTimePicker
+    Friend WithEvents Report1 As Report
+End Class
